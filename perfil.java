8d126185032a83b719947d87a9a768279499d189
RaianInfante <raianzinn1@gmail.com>
2025-02-24 21:21:43 -0300

    Commit da Atividade

---
+++ perfil.java
@@ -0,0 +1,7 @@
+public class perfil {
+    public static void main(String[] args) {
+        System.out.println("Meu nome é Raian");
+        System.out.println("Tenho 19 anos");
+        System.out.println("Gosto de estudar programação");
+    }
+}
